bc87625227ba287fa1744926fa7f8f3c232babd7
xiaozong6 <zongboli0814@126.com>
2025-04-23 22:28:30 -0700

    dsad

--- kernel/main.c
+++ kernel/main.c
@@ -28,7 +28,7 @@ main()
     iinit();         // inode cache
     fileinit();      // file table
     virtio_disk_init(); // emulated hard disk
-    userinit();      // first user process
+    //userinit();      // first user process
     __sync_synchronize();
     started = 1;
   } else {

db82d2efd874bdac69226c4d488c1a5119ee4de6
xiaozong6 <zongboli0814@126.com>
2025-05-10 20:07:33 -0700

    lab2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,8 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET];
+
 
 double
 now()
@@ -38,8 +40,9 @@ insert(int key, int value, struct entry **p, struct entry *n)
 static 
 void put(int key, int value)
 {
-  int i = key % NBUCKET;
 
+  int i = key % NBUCKET;
+    pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,11 +56,13 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
 get(int key)
 {
+  //pthread_mutex_lock(&lock);
   int i = key % NBUCKET;
 
 
@@ -65,6 +70,7 @@ get(int key)
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  //pthread_mutex_unlock(&lock);
 
   return e;
 }
@@ -102,6 +108,9 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  for(int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&lock[i], NULL);
+  }
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
